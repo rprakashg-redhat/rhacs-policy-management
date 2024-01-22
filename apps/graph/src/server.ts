03ee43d2c5991ea3227e4976fce3028102997695
Ram <rgopinat@redhat.com>
2024-01-21 21:29:55 -0800

    commenting out cache

--- apps/graph/src/server.ts
+++ apps/graph/src/server.ts
@@ -5,7 +5,7 @@ import { context } from './context';
 export const server = new ApolloServer({ 
     schema: schema, 
     context: context,
-    cache: "bounded",
+    //cache: "bounded",
     persistedQueries: false,
     introspection: true
 });
